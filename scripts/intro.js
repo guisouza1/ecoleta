c51e626bd3e8b812503c91bfa911c62a834b0904
Guilherme Medeiros <guilhermemedeirosdesouza@gmail.com>
2020-06-02 22:28:19 -0300

    completed registration screen (front end)

---
+++ scripts/intro.js
@@ -0,0 +1,98 @@
+//comments
+//document.write('Hello')
+
+// ==================================================
+
+//variaveis, tipos de dados
+
+// let possibilita declarar variavel limitando seu escopo no bloco
+
+//let myvar = "he"
+//const myconst = 'he'
+
+//document.write(myconst+ myvar)
+
+// ==================================================
+
+//STRING - "", '', ``
+
+
+//"string1"
+//'string2'
+//`string3`
+
+// ==================================================
+
+//NUMBER
+
+
+//const n1 = 1
+//const n2= 12
+//document.write(n1+n2)
+
+// ==================================================
+
+//BOOLEAN - true ou false
+
+
+//const btrue=true
+//const bfalse= false
+//document.write(bfalse)
+
+// ==================================================
+
+//objetos possuem propriedades e funcionalidades
+
+
+//const pessoa = {
+ // altura: "1,90m",
+//  idade: 24,
+ // solteiro: true,
+//  correr(){
+//    return "run forest"
+ // }
+//}
+
+//document.write(pessoa.correr())
+
+// ==================================================
+
+//ARRAY - vetores (coleção de dados)
+
+//const colecaoDebolinhas = ["blue", "green", 1, {name:"my name"}]
+//document.write(colecaoDebolinhas[3].name)    
+
+// ==================================================
+
+//FUNÇÕES - funcionalidades - atalhos - reuso de codigo
+
+//function saymyname(name){
+//  document.write(name)
+//}
+
+
+//saymyname("bruno")
+//saymyname("maicon")
+//saymyname("guilherme")
+//saymyname("brenda")
+
+// ==================================================
+
+//CONDICIONAIS
+
+//const notafinal = 7
+
+//if(notafinal < 5){
+// document.write("reprovado")
+//} else{
+//  document.write("aprovado")
+//}
+
+// ==================================================
+
+//LOOP OU REPETIÇÕES
+// ex=(i++) é como se fosse i+1
+
+//for (i = 0; i < 10; i++){
+//    document.write(`<p> ${i} </p>`)
+//  }
